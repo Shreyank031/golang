cd127ec45ad1aee16b36614b6701c7722814873a
Shreyank031 <shreyankchandu@gmail.com>
2024-04-29 21:03:34 +0530

    Add MovieAsWatched

--- mongoapi/controller/controller.go
+++ mongoapi/controller/controller.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/Shreyank031/mongoapi/model"
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -132,4 +133,24 @@ func GetAllMovies(w http.ResponseWriter, r *http.Request) {
 	allMovies := getAllMovies()
 	json.NewEncoder(w).Encode(allMovies)
 
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	var movie model.Netflix
+	_ := json.NewDecoder(r.Body).Decode(&movie)
+	insertOneMovie(movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func MarkedAsWatched(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	params := mux.Vars(r)
+	updateOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params)
 }
