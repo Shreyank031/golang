409610908a59c13c25172dd1dc761c5c3e45f7ce
Shreyank031 <shreyankchandu@gmail.com>
2024-04-30 17:35:07 +0530

    close(channel)

--- goroutines/channel/main.go
+++ goroutines/channel/main.go
@@ -8,19 +8,27 @@ import (
 func main() {
 	fmt.Println("All about Channels in golang")
 
-	myCh := make(chan int)
+	// Create a buffered channel with a capacity of 2
+	myCh := make(chan int, 2) //two channels
+	// Create a WaitGroup to synchronize goroutines
 	wg := &sync.WaitGroup{}
 
-	wg.Add(2)
+	wg.Add(2) // Increment the WaitGroup counter by 2 for the two goroutines
 	go func(ch chan int, wg *sync.WaitGroup) {
-		fmt.Println(<-ch)
-		fmt.Println(<-ch)
+
+		val, isChannelOpenn := <-ch
+		fmt.Println(isChannelOpenn)
+		fmt.Println(val)
+
+		// fmt.Println(<-ch)
+		// fmt.Println(<-ch)
 		wg.Done()
 	}(myCh, wg)
 
 	go func(ch chan int, wg *sync.WaitGroup) {
-		ch <- 5
-		ch <- 9
+		close(ch)
+		// ch <- 5
+		// ch <- 9
 		wg.Done()
 	}(myCh, wg)
 
